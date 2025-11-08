c567aca03e35119683f25484fb08a597c1b42c66
Muze <mark46716@gmail.com>
2025-11-08 17:39:49 -0300

    Agrega bloques de vidrio y piedra

---
+++ Assets/Scripts/LanzarPersonaje.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class LanzarPersonaje : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+    [SerializeField] private float fuerzaLanzamiento = 300f;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private Camera main;
+    private Vector2 startPosition, clampedPosition;
+    private float startRotation;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        startPosition = transform.position;
+        startRotation = rb.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        if (main != null)
+        {
+            Vector2 dragPosition = main.ScreenToWorldPoint(Input.mousePosition);
+            clampedPosition = dragPosition;
+
+            float dragDistance = Vector2.Distance(startPosition, dragPosition);
+
+            //Limita la distancia de arrastre maxima para los personajes
+            if (dragDistance > maxDistance)
+            {
+                clampedPosition = startPosition + (dragPosition - startPosition).normalized * maxDistance; //Suma la pos inicial + un vector con tamaño equivalente a maxDistance
+            }
+
+            transform.position = clampedPosition;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Vector2 direccionLanzamiento = startPosition - clampedPosition;
+        rb.AddForce(direccionLanzamiento * fuerzaLanzamiento);
+        Invoke("resetPosition", 3f);
+    }
+
+    private void resetPosition()
+    {
+        transform.position = startPosition;
+        rb.rotation = startRotation;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.linearVelocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        main.GetComponent<CameraFollowing>().resetPosition();
+    }
+}
