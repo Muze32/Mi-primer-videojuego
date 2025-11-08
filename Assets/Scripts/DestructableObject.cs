5c39cf839e0a6da3f1ce38ca2042d708e3e618d7
Muze <mark46716@gmail.com>
2025-11-08 19:24:24 -0300

    Agrega animacion al destruir un objeto

--- Assets/Scripts/DestructableObject.cs
+++ Assets/Scripts/DestructableObject.cs
@@ -1,18 +1,42 @@
 using UnityEngine;
 
-
 public class DestructableObject : MonoBehaviour
 {
+    [SerializeField] private Sprite resistanceMedium;
+    [SerializeField] private Sprite resistanceLow;
     [SerializeField] private float resistance;
+    private float resistanceIni;
+    private SpriteRenderer spriteR;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        resistanceIni = resistance;
+        spriteR = GetComponent<SpriteRenderer>();
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.relativeVelocity.magnitude > resistance)
+        float velColision = collision.relativeVelocity.magnitude;
+
+        //Destruye el objeto si la velocidad es mayor a su resistencia
+        if (velColision > resistance)
         {
+            animator.SetTrigger("dead");
             Destroy(gameObject, 0.2f);
         }
         else
         {
-            resistance -= collision.relativeVelocity.magnitude;
+            resistance -= velColision;
+            //Cambia los sprites de los objetos segun la resistencia
+            if(resistance <= resistanceIni / 3)
+            {
+                spriteR.sprite = resistanceLow;
+            } else if(resistance > resistanceIni / 3 && resistance <= resistanceIni * 2/3)
+            {
+                spriteR.sprite = resistanceMedium;
+            }
         }
     }
 }
